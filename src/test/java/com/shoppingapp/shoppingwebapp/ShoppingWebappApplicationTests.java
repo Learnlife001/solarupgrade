33f63f37df9e86f2d16d18780f91901efd96e552
learnlife001 <ilearnlife23@gmail.com>
2025-02-22 09:20:15 +0800

    newly added

---
+++ src/test/java/com/shoppingapp/shoppingwebapp/ShoppingWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingapp.shoppingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
